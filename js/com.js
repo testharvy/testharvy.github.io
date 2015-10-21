64b1164ffa56ed38590b33510196f8d5a1ce8b9b
Andreev Konstantin <testharvy@gmail.com>
2015-10-21 12:22:54 +0300

    mobile version (swipe)

--- js/com.js
+++ js/com.js
@@ -15,6 +15,8 @@ $(function(){
 		a =  width_center-x*300 ;
 		b =  height_center-y*300 ;
 		$slideshow.css({ transform: 'translateX('+a+'px) translateY('+b+'px)' }); 
+		current_x = x;
+		current_y = y;
     };
     var makeActive = function(open) {
 		for(var i=0; i<open.length; i++){
@@ -32,11 +34,53 @@ $(function(){
 	        		$(el).addClass('-show');
 	        		makeActive(open);
 	        	}
-	            goTo(x, y, open);
+	            goTo(x, y);
         	}
         });
     });	
 
     goTo(0,0 );
     makeActive(["1,0"]);
+
+    var trySwipe = function(x,y) {
+    	var $tmp_slide = $slide.filter('[data-x='+x+'][data-y='+y+']'),
+    		open = $tmp_slide.data('open').split(';');
+
+    	if($slide.filter('[data-x='+x+'][data-y='+y+']').hasClass('-canmove')){
+    		goTo(x,y);
+    		$tmp_slide.addClass('-show');
+    		makeActive(open);
+    	}
+    	
+   	};
+
+    var goLeft = function() {
+    	var x = current_x+1,
+    		y = current_y;
+    	trySwipe(x,y);
+    	
+   	};
+   	var goRight = function() {
+    	var x = current_x-1,
+    		y = current_y;
+    	trySwipe(x,y);
+    	
+   	};
+
+   	var goUp = function() {
+   		var x = current_x,
+    		y = current_y+1;
+    	trySwipe(x,y);
+   	};
+   	var goDown = function() {
+   		var x = current_x,
+    		y = current_y-1;
+    	trySwipe(x,y);
+   	};
+
+    $slideshow.on( "swipeleft", goLeft );
+    $slideshow.on( "swiperight", goRight);
+    $slideshow.on( "swipeup", goUp );
+   	$slideshow.on( "swipedown", goDown );
+
 });
